1cc5620f53836aeaaea76d4bd4971701be1de478
Linuxgeekforgeeks <mahmoudalisaaa@gmail.com>
2024-08-31 16:34:48 -0700

    creating Circle Button Component

--- src/components/shared/Navigation/Navbar/Navbar.tsx
+++ src/components/shared/Navigation/Navbar/Navbar.tsx
@@ -1,8 +1,11 @@
+import NavigationHeader from "../NavigationHeader"
 
 
 function Navbar() {
   return (
-    <div className="fixed bg-stone-950 w-full z-20 h-16 px-2 flex-row justify-between items-center">Navbar</div>
+    <div className="fixed bg-stone-950 w-full z-20 h-16 px-2 flex-row justify-between items-center">
+        <NavigationHeader/>
+    </div>
   )
 }
 
---
+++ src/components/shared/Navigation/NavigationHeader.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { MdMenu } from "react-icons/md";
+import { IconButton } from "../IconButton";
+
+function NavigationHeader() {
+  return (
+    <div className="flex flex-row items-center">
+        <IconButton>
+
+      <MdMenu className="h-6 w-6" />
+        </IconButton>
+    </div>
+  );
+}
+
+export default NavigationHeader;
