b6d11f7284354aa49772efb3ec22d233c4cd9a38
Linuxgeekforgeeks <mahmoudalisaaa@gmail.com>
2024-08-31 16:04:20 -0700

    I make Navigation Fixed which tend to overlap

---
+++ src/components/shared/Navigation/Navbar/Navbar.tsx
@@ -0,0 +1,9 @@
+
+
+function Navbar() {
+  return (
+    <div className="fixed bg-stone-950 w-full z-20 h-16 px-2 flex-row justify-between items-center">Navbar</div>
+  )
+}
+
+export default Navbar
---
+++ src/components/shared/Navigation/Navigation.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './Navbar/Navbar'
+
+function Navigation() {
+  return (
+    <>
+        <Navbar/>
+    </>
+  )
+}
+
+export default Navigation
